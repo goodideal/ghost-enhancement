9b958e2aecfecc2a98bee0e7a661a26b353af19a
jerry <jerry@wjz.im>
2020-02-17 22:53:53 +0800

    init

---
+++ app/controller/post.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const Controller = require("egg").Controller;
+
+class HomeController extends Controller {
+  async getCount() {
+    const { ctx } = this;
+    const { slug } = ctx.params;
+    ctx.body = ctx.db.get("posts." + slug).value();
+  }
+
+  async increaseCount() {
+    const { ctx } = this;
+    const { slug } = ctx.params;
+    ctx.db.update("posts." + slug, n => (n ? n : 0) + 1).write();
+    ctx.body = ctx.db.get("posts." + slug).value();
+  }
+}
+
+module.exports = HomeController;
