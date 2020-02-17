339b3e31c49d1a893d46e1e1b1e2e192556face6
jerry <jerry@wjz.im>
2020-02-17 23:22:16 +0800

    npm run for docker

--- app/controller/post.js
+++ app/controller/post.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-const Controller = require("egg").Controller;
+const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async getCount() {
     const { ctx } = this;
     const { slug } = ctx.params;
-    ctx.body = ctx.db.get("posts." + slug).value();
+    ctx.body = ctx.db.get('posts.' + slug).value();
   }
 
   async increaseCount() {
     const { ctx } = this;
     const { slug } = ctx.params;
-    ctx.db.update("posts." + slug, n => (n ? n : 0) + 1).write();
-    ctx.body = ctx.db.get("posts." + slug).value();
+    ctx.db.update('posts.' + slug, n => (n ? n : 0) + 1).write();
+    ctx.body = ctx.db.get('posts.' + slug).value();
   }
 }
 
