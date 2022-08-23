810a31ad8750740c0e410f79d7b7405b2b03e6e2
JerryZ <z@wjz.im>
2022-08-23 21:56:20 +0800

    fix config path

--- config/config.default.js
+++ config/config.default.js
@@ -29,13 +29,13 @@ module.exports = appInfo => {
       port: "3306",
       user: "root",
       password: "ghost",
-      database: "ghost"
+      database: "ghost",
+      isPromise: true
     },
     // 是否加载到 app 上，默认开启
     app: true,
     // 是否加载到 agent 上，默认关闭
     agent: false,
-    isPromise: true
   };
 
   config.contextPath = process.env.CONTEXT_PATH || "/enhance"
--- config/config.prod.js
+++ config/config.prod.js
@@ -38,14 +38,13 @@ module.exports = appInfo => {
       port: MYSQL_PORT || "3306",
       user: MYSQL_USER || "blog",
       password: MYSQL_PASSWORD || "password",
-      database: MYSQL_DATABASE || "blog"
+      database: MYSQL_DATABASE || "blog",
+      isPromise: true
     },
     // 是否加载到 app 上，默认开启
     app: true,
     // 是否加载到 agent 上，默认关闭
     agent: false,
-
-    isPromise: true
   };
 
   return {
